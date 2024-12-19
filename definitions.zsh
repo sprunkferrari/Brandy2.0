0b3415e254a64c9aaf2fb39ce4b0556b422b6b24
Francesco Ferrari <148579474+sprunkferrari@users.noreply.github.com>
2024-12-19 13:53:18 +0100

    Update definitions.zsh

--- definitions.zsh
+++ definitions.zsh
@@ -1,23 +1,17 @@
 #!/bin/zsh
 # Script used for defining all the fixed variables in the system.
 #####
-GATEWAY="192.168.1.1" > $1
-SPRUNK_MINI="192.168.1.10" > $2
-JURI_MINI="192.168.1.11" > $3
-SPRUNK_RASPI_P6="192.168.1.20" > $4
-SPRUNK_RASPI_RACK="192.168.1.21" > $5
-JACK_RASPI="192.168.1.23" > $6
-JURI_RASPI="192.168.1.24" > $7
-X32_DANTE="192.168.1.30" > $8
-X32_CONTROL="192.168.1.31" > $9
-SPRUNK_DN30R="192.168.1.40" > $10
-SPRUNK_DN30T="192.168.1.41" > $11
-JURI_DN30T="192.168.1.42" > $12
-  
-  
-
-for i in $*
-  do export $i
-  done
+export GATEWAY="192.168.1.1"
+export SPRUNK_MINI="192.168.1.10"
+export JURI_MINI="192.168.1.11"
+export SPRUNK_RASPI_P6="192.168.1.20"
+export SPRUNK_RASPI_RACK="192.168.1.21"
+export JACK_RASPI="192.168.1.23"
+export JURI_RASPI="192.168.1.24"
+export X32_DANTE="192.168.1.30"
+export X32_CONTROL="192.168.1.31"
+export SPRUNK_DN30R="192.168.1.40"
+export SPRUNK_DN30T="192.168.1.41"
+export JURI_DN30T="192.168.1.42"
 
 exit
