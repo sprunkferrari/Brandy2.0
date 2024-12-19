2386c9615a78f1105646ab9402af570509533693
Francesco Ferrari <148579474+sprunkferrari@users.noreply.github.com>
2024-12-19 11:30:44 +0100

    Create definitions.zsh

---
+++ definitions.zsh
@@ -0,0 +1,23 @@
+#/bin/zsh
+# Script used for defining all the fixed variables in the system.
+#####
+$GATEWAY=192.168.1.1
+$SPRUNK_MINI=192.168.1.10
+$JURI_MINI=192.168.1.11
+$SPRUNK_RASPI_P6=192.168.1.20
+$SPRUNK_RASPI_RACK=192.168.1.21
+$JACK_RASPI=192.168.1.23
+$JURI_RASPI=192.168.1.24
+$X32_DANTE=192.168.1.30
+$X32_CONTROL=192.168.1.31
+$SPRUNK_DN30R=192.168.1.40
+$SPRUNK_DN30T=192.168.1.41
+$JURI_DN30T=192.168.1.42
+  
+  
+
+for i in #list
+  do export $i
+  done
+
+exit
