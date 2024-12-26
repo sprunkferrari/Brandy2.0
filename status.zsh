65615a94794408dd8213b333b6d0501fe14ba8a4
Francesco Ferrari <148579474+sprunkferrari@users.noreply.github.com>
2024-12-26 15:00:46 +0100

    Update status.zsh

--- status.zsh
+++ status.zsh
@@ -13,23 +13,14 @@ set - $(printenv ALL_DEVICES)
 COUNT_MISSING="0"
 COUNT_CONNECTED="0"
 
-for DEVICE ; do
-    eval IP_ADDRESS=$"$DEVICE"
-    if ! ping -o -c 3 -t 2 $IP_ADDRESS > /dev/null ; then
-        printf "$DEVICE is missing. "
-        COUNT_MISSING=$(("$COUNT_MISSING" + 1))
-        export ""$DEVICE"_STATUS"="MISSING"
-        else COUNT_CONNECTED=$(("$COUNT_CONNECTED" + 1))
-        export ""$DEVICE"_STATUS"="CONNECTED"
-    fi
-done
+ipsub()
+{
+    local IP_ADDRESS
+    eval IP_ADDRESS=$"$1"
+    ping -o -c 3 -t 2 -q $IP_ADDRESS > /dev/null
+}
 
-printf "\n"
-printf ""$COUNT_CONNECTED"/"$#" devices connected\n"
-if [[ COUNT_MISSING > 0 ]]; then
-    printf "$COUNT_MISSING devices missing\n"
 
-fi
 
 sleep 1
 printf "-----------------\n"
@@ -43,6 +34,12 @@ DN_LIST=$(dante-cli list-devices)
 
 printf "--DEVICE------NET STATUS------VH STATUS-------DANTE STATUS--\n"
 for DEVICE ; do
+if pingsub $DEVICE ; then
+export COUNT_CONNECTED=$(("$COUNT_CONNECTED" + 1)
+export ""$DEVICE"_STATUS"="CONNECTED"
+else
+COUNT_MISSING=$(("$COUNT_MISSING" + 1))
+export ""$DEVICE"_STATUS"="MISSING"
 if  [[ $DEVICE =~ $DN_LIST ]] ; then
         export ""$DEVICE"_DN_STATUS"="CONNECTED"
         else export ""$DEVICE"_DN_STATUS"="MISSING"
@@ -53,3 +50,10 @@ if  [[ $DEVICE =~ $VH_LIST ]] ; then
 fi
     printf "$DEVICE     $(printenv "$DEVICE"_STATUS)    "$(printenv "$DEVICE"_VH_STATUS)     $(printenv "$DEVICE"_DN_STATUS)"
 
+printf "\n"
+printf ""$COUNT_CONNECTED"/"$#" devices connected\n"
+if [[ COUNT_MISSING > 0 ]]; then
+    printf "$COUNT_MISSING devices missing\n"
+exit 1
+else exit 0
+fi
