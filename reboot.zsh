6dc0a32e7dfdc2ce917adba713a418a407dc5d0b
Francesco Ferrari <148579474+sprunkferrari@users.noreply.github.com>
2024-12-27 14:14:17 +0100

    Update reboot.zsh

--- reboot.zsh
+++ reboot.zsh
@@ -1,7 +1,37 @@
 #!/bin/zsh
 
-#  reboot.zsh
+#  terminate+reboot.zsh
 #  
 #
 #  Created by Francesco Ferrari on 20/12/24.
 #  
+printf "-------------\n"
+printf "Ready to Terminate and Reboot\n"
+printf "-------------\n"
+
+. ./definitions.zsh
+set - $(printenv PC_DEVICES)
+
+#Quitting local
+
+zsh ./quit.zsh
+
+#Quitting J
+if pingsub 192.168.1.11 ; then
+    printf "Quitting JURI_MINI\n"
+    ssh sprunk@192.168.1.11 ./quit.zsh
+fi
+
+sleep 5
+#Reboot remotes
+for DEVICE ; do
+        eval IP_ADDRESS=$"$DEVICE"
+        if pingsub $IP_ADDRESS ; then
+        printf "Rebooting $DEVICE\n"
+        ssh sprunk@"$IP_ADDRESS" shutdown -r now
+        fi
+done
+
+#Reboot local
+printf "Rebooting local...\n"
+shutdown -r 5
