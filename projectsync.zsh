f7bd6fb292a08079c36fd56188b5dfcbcf06a37c
Francesco Ferrari <ferrari.francesco.f@gmail.com>
2025-01-24 00:04:38 +0100

    Up

--- projectsync.zsh
+++ projectsync.zsh
@@ -1,9 +1,10 @@
 # /bin/zsh
 ############
 # Brandy2.0 by Francesco Ferrari
-# Project Sync: a simple tool for automating copy of the Project Folders to multiple destinations
+# Project Sync: a simple tool for automating copy of the Project Folder to multiple destinations
 ############
 printf ----------------/n
 . ./definitions.zsh
-printf ---Project Sync----/n
-
+printf ---Project Sync----
+-----------------
+Choose the sync direction
