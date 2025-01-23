f579eb0f540a6bf6101f2b8f38349c28c4f4a5cc
Francesco Ferrari <148579474+sprunkferrari@users.noreply.github.com>
2025-01-23 23:48:52 +0100

    Create projectsync.zsh

---
+++ projectsync.zsh
@@ -0,0 +1,9 @@
+# /bin/zsh
+############
+# Brandy2.0 by Francesco Ferrari
+# Project Sync: a simple tool for automating copy of the Project Folders to multiple destinations
+############
+printf ----------------/n
+. ./definitions.zsh
+printf ---Project Sync----/n
+
