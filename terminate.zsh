e5690eff0cd09df05013c3076106b811436a1d81
Francesco Ferrari <148579474+sprunkferrari@users.noreply.github.com>
2025-01-24 15:37:26 +0100

    Update terminate.zsh

--- terminate.zsh
+++ terminate.zsh
@@ -21,23 +21,48 @@ else	while [[ $? == 1 ]]; do
 			printf "\nFailed. Try again? y/n ->"
             read ANSWER
             case $ANSWER in
-            	( y ) continue;;
+            	( y ) printf "Quitting local..." && shortcuts Quit ;;
                 ( n ) break;;
                 ( * ) printf "Not allowed.\n" && continue;;
             esac
         done
+fi
 #Quitting J
 if pingsub "$JURI_MINI" ; then
     printf "Quitting JURI_MINI..."
     ssh sprunk@"$(printenv JURI_MINI)" shortcuts Quit
+    if [[ $? == 0 ]] ; 
+		then printf "\tSuccess\n"
+		else	while [[ $? == 1 ]]; do
+					printf "\nFailed. Try again? y/n ->"
+            		read ANSWER
+            		case $ANSWER in
+            		( y ) printf "Quitting local..." && shortcuts Quit ;;
+                ( n ) break;;
+                ( * ) printf "Not allowed.\n" && continue;;
+            	esac
+        		done
+	fi
 fi
 sleep 5
 #Shutting remotes
 for DEVICE ; do
         eval IP_ADDRESS=$"$DEVICE"
         if pingsub $IP_ADDRESS ; then
-        ssh sprunk@"$IP_ADDRESS" shutdown -h now
-        printf "Shutting down $DEVICE\n"
+        	printf "Shutting down $DEVICE ..."
+    		ssh sprunk@"$IP_ADDRESS" shutdown -h now
+        		if [[ $? == 0 ]] ; 
+					then printf "\tSuccess\n"
+					else	while [[ $? == 1 ]]; do
+								printf "\nFailed. Try again? y/n ->"
+            					read ANSWER
+            				case $ANSWER in
+            					( y ) printf "Shutting down $DEVICE ..." && ssh sprunk@"$IP_ADDRESS" shutdown -h now ;;
+                ( n ) break;;
+                ( * ) printf "Not allowed.\n" && continue;;
+            				esac
+        					done
+                fi
         fi
 done
 
